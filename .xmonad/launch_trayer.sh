bcdd09ab4f7383ea3504c80d8f932af6d7758953
Andreas Arvidsson <andreas.arvidson@gmail.com>
2015-11-13 11:44:17 +0000

    Reorganize dzen2 bar and trayer for more consistent look.

--- .xmonad/launch_trayer.sh
+++ .xmonad/launch_trayer.sh
@@ -4,4 +4,4 @@ for PID in `pgrep -x trayer`; do
     kill ${PID} > /dev/null &
 done
 
-trayer --edge top --align right --height 18 --width 3 --transparent true --alpha 0 --tint 0x1b1d1e &
+trayer --edge top --align right --height 18 --width 4 --transparent true --alpha 0 --tint 0x1b1d1e --distancefrom right --distance 320 &
