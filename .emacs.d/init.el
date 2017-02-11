41dee93b84a3feebe69e15898fd314d9f548f7f9
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-02-11 21:19:45 +0100

    Add "o" keybinding inside neotree

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,6 +19,9 @@
   :ensure t)
 (setq neo-window-width 40)
 (define-key evil-normal-state-map (kbd "<backspace>") 'neotree-toggle)
+(add-hook 'neotree-mode-hook
+	  (lambda ()
+	    (define-key evil-normal-state-local-map (kbd "o") 'neotree-enter)))
 
 ; helm
 (use-package helm
