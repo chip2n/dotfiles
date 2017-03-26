347f41af9d2a17305915ab47937d87a0db1d6282
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-03-26 12:27:18 +0200

    Add markdown and yaml files to emacs configuration dir

---
+++ .emacs.d/scripts/markdown.el
@@ -0,0 +1,5 @@
+(provide 'markdown)
+
+;; markdown-mode: http://jblevins.org/projects/markdown-mode/
+(use-package markdown-mode
+  :ensure t)
---
+++ .emacs.d/scripts/yaml.el
@@ -0,0 +1,5 @@
+(provide 'yaml)
+
+;; yaml-mode: https://github.com/yoshiki/yaml-mode
+(use-package yaml-mode
+  :ensure t)
