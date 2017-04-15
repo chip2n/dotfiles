93bbf3e9cc9cab8e4a139a91487ae30756bafce9
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-04-15 23:23:31 +0200

    Add commented code for smart-mode-line theme stuff in emacs config

--- .emacs.d/scripts/init-theme.el
+++ .emacs.d/scripts/init-theme.el
@@ -34,3 +34,27 @@
 (set-frame-parameter nil 'internal-border-width 10)
 (set-frame-parameter nil 'left-fringe 10)
 (set-frame-parameter nil 'right-fringe 10)
+
+;;(use-package smart-mode-line
+;;  :ensure t)
+;;(setq sml/no-confirm-load-theme t)
+
+
+
+;;(deftheme smart-mode-line-chiptheme "Chip theme for smart-mode-line.")
+;;
+;;(custom-theme-set-faces
+;; 'smart-mode-line-chiptheme
+;; '(mode-line-buffer-id ((t :inherit sml/filename :foreground nil :background nil))) 
+;; '(mode-line-inactive ((t :foreground "gray60" :background "#404045" :inverse-video nil)))
+;; '(mode-line     ((t :foreground "gray60" :background "#ff0000" :inverse-video nil)))
+;; '(sml/global    ((t :foreground "gray50" :inverse-video nil)))
+;; '(sml/modes     ((t :inherit sml/global :foreground "White")))
+;; '(sml/filename  ((t :inherit sml/global :foreground "#eab700" :weight bold)))
+;; '(sml/prefix    ((t :inherit sml/global :foreground "#bf6000")))
+;; '(sml/read-only ((t :inherit sml/not-modified :foreground "DeepSkyBlue")))
+;; '(persp-selected-face ((t :foreground "ForestGreen" :inherit sml/filename)))
+;; '(helm-candidate-number ((t :foreground nil :background nil :inherit sml/filename))))
+;;
+;;(setq sml/theme 'chiptheme)
+;;(sml/setup)
