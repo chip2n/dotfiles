fe153e80c218584030d2aafb7338f52d6b0308c4
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-04-15 23:11:24 +0200

    Add vim magic to helm via hydra in emacs config

--- .emacs.d/scripts/init-helm.el
+++ .emacs.d/scripts/init-helm.el
@@ -18,6 +18,74 @@
 (use-package helm-ag
   :ensure t)
 
+;; vim movement in helm
+(defun helm-like-unite/pre ()
+  (set-cursor-color "#ffffff"))
+(defun helm-like-unite/post ()
+  (set-cursor-color "#ffffff"))
+(defhydra helm-like-unite
+    (:pre helm-like-unite/pre
+     :post helm-like-unite/post
+     :hint nil
+     :color amaranth)
+"
+    Nav    Scroll  Mark             Other            Quit
+ ╭─────────────────────────────────────────────────────╮
+   ^ ^ _k_ ^ ^     _K_     [_m_] mark         [_v_] view         [_q_] quit
+   _h_ ^+^ _l_     ^↕^     [_t_] toggle all   [_d_] delete
+   ^ ^ _j_ ^ ^     _J_     [_u_] unmark all   [_f_] follow: %(helm-attr 'follow)
+ ╰─────────────────────────────────────────────────────╯
+"
+  ;; arrows
+  ("h" helm-beginning-of-buffer)
+  ("j" helm-next-line)
+  ("k" helm-previous-line)
+  ("l" helm-end-of-buffer)
+  ;; beginning/end
+  ("g" helm-beginning-of-buffer)
+  ("G" helm-end-of-buffer)
+  ;; scroll
+  ("K" helm-scroll-other-window-down)
+  ("J" helm-scroll-other-window)
+  ;; mark
+  ("t" helm-toggle-all-marks)
+  ("m" helm-toggle-visible-mark)
+  ("u" helm-unmark-all)
+  ;; misc
+  ("q" nil)
+  ("v" helm-execute-persistent-action)
+  ("f" helm-follow-mode)
+  ("d" helm-persistent-delete-marked)
+  ("/" (lambda ()
+          (interactive)
+          (execute-kbd-macro [?\C-s]))
+       "search")
+  ("<escape>" keyboard-escape-quit "quit helm")
+  ("?" helm-help "help"))
+(define-key helm-map (kbd "<escape>") 'helm-like-unite/body)
+
+(defun helm-persistent-delete-marked ()
+  "Kill buffer without quitting helm."
+  (interactive)
+  (if (equal (cdr (assoc 'name (helm-get-current-source)))
+             "Buffers")
+      (with-helm-alive-p
+        (helm-attrset 'kill-action
+                      '(helm-persistent-kill-buffers . never-split))
+        (helm-execute-persistent-action 'kill-action))
+    (user-error "Only works for buffers")))
+
+(defun helm-persistent-kill-buffers (_buffer)
+  (unwind-protect
+       (dolist (b (helm-marked-candidates))
+         (helm-buffers-persistent-kill-1 b))
+    (with-helm-buffer
+      (setq helm-marked-candidates nil
+            helm-visible-mark-overlays nil))
+    (helm-force-update (helm-buffers--quote-truncated-buffer
+                        (helm-get-selection)))))
+
+
 
 ;; -----------------------------------------------------------------------
 ;; Add keybinding for opening buffers in splits
