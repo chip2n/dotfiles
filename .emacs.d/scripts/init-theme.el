b980f29aecc43ae15bc98bf6a64b851c437bce4a
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-04-15 23:11:50 +0200

    Tweak cursor in emacs config

--- .emacs.d/scripts/init-theme.el
+++ .emacs.d/scripts/init-theme.el
@@ -24,3 +24,8 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+;; disable blinking cursor (caused some flicker in images when using eww)
+(blink-cursor-mode 0)
+
+(set-cursor-color "#ffffff")
