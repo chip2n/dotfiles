bfd88335902e453f5824a4bd9c2d0c559bc59d89
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-04-22 22:14:20 +0200

    Add better file backup to emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -163,6 +163,23 @@
 (setq smooth-scroll-margin 5)
 (smooth-scrolling-mode 1)
 
+;; file backup
+(defvar --backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p --backup-directory))
+        (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
+
 ;; enable line numbering
 ;;(global-linum-mode 1)
 
