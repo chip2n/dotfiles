8fac447315e55f7994eb1ea83beca0362bafe5b8
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-05-16 20:45:27 +0200

    Add background color to mode line in emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -228,6 +228,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "blue" :foreground "#bbc2cf"))))
  '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
