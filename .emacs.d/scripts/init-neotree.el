f953ee1d8835598883aa85cf5d53e044e4ec582c
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-05-16 20:34:59 +0200

    Add better keybindings to neotree in emacs configuration

--- .emacs.d/scripts/init-neotree.el
+++ .emacs.d/scripts/init-neotree.el
@@ -7,12 +7,13 @@
 (define-key evil-normal-state-map (kbd "S-<backspace>") 'neotree-project-dir)
 (add-hook 'neotree-mode-hook
 	  (lambda ()
+	    (define-key evil-normal-state-local-map (kbd "m") 'chip-hydra-neotree/body)
 	    (define-key evil-normal-state-local-map (kbd "o") 'neotree-enter)
-	    (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
-	    (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
+	    (define-key evil-normal-state-local-map (kbd "O") 'neotree-quick-look)
 	    (define-key evil-normal-state-local-map (kbd "r") 'neotree-refresh)
 	    (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
 	    (define-key evil-normal-state-local-map (kbd "i") 'neotree-enter-horizontal-split)
+	    (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
 	    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
 
 ;; stolen from: http://nadeemkhedr.com/emacs-tips-and-best-plugins-to-use-with-evil-mode/
@@ -31,3 +32,22 @@
 
 ;; open neotree at current working directory
 (setq neo-smart-open t)
+
+;; hydra for neotree file operations
+(defhydra chip-hydra-neotree
+    (:color amaranth
+     :hint nil)
+"
+   Nav^            ^Quit
+ ╭─^──^───────────^───^─────╮
+   [_a_] add     [_q_] quit
+   [_c_] copy
+   [_m_] move
+   [_d_] delete
+ ╰─^──^───────────^───^─────╯
+"
+  ("a" neotree-create-node :exit t)
+  ("c" neotree-copy-node :exit t)
+  ("m" neotree-rename-node :exit t)
+  ("d" neotree-delete-node :exit t)
+  ("q" nil))
