028d9927401004a2a10a9f888c80d63f7a8d6aa7
Andreas Arvidsson <andreas.arvidson@gmail.com>
2017-12-23 14:13:46 +0100

    Add emacs configuration for EXWM

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,6 +15,90 @@
 (require 'use-package)
 ;; -----------------------------------------------------------
 
+
+
+(use-package exwm
+  :ensure t)
+(require 'exwm-config)
+
+(setq exwm-workspace-number 2)
+
+(require 'exwm-randr)
+(setq exwm-randr-workspace-output-plist '(0 "DVI-D-0" 1 "DVI-I-0"))
+(add-hook 'exwm-randr-screen-change-hook
+          (lambda ()
+            (start-process-shell-command
+             "xrandr" nil "xrandr --output DVI-D-0 --left-of DVI-I-0 --auto")))
+(exwm-randr-enable)
+
+
+
+(exwm-input-set-key (kbd "s-SPC") 'helm-M-x)
+
+(push ?\s-w exwm-input-prefix-keys)
+(push ?\s-e exwm-input-prefix-keys)
+(exwm-input-set-key (kbd "s-w") (lambda () (interactive) (exwm-workspace-switch 0)))
+(exwm-input-set-key (kbd "s-e") (lambda () (interactive) (exwm-workspace-switch 1)))
+
+(push ?\s-h exwm-input-prefix-keys)
+(push ?\s-k exwm-input-prefix-keys)
+(push ?\s-j exwm-input-prefix-keys)
+(push ?\s-l exwm-input-prefix-keys)
+(exwm-input-set-key (kbd "s-h") 'evil-window-left)
+(exwm-input-set-key (kbd "s-k") 'evil-window-up)
+(exwm-input-set-key (kbd "s-l") 'evil-window-right)
+(exwm-input-set-key (kbd "s-j") 'evil-window-down)
+
+(push ?\s-b exwm-input-prefix-keys)
+(exwm-input-set-key (kbd "s-b") 'helm-buffers-list)
+
+(push ?\s-\C-h exwm-input-prefix-keys)
+(push ?\s-\C-j exwm-input-prefix-keys)
+(push ?\s-\C-k exwm-input-prefix-keys)
+(push ?\s-\C-l exwm-input-prefix-keys)
+(exwm-input-set-key (kbd "s-C-h") 'split-window-right)
+(exwm-input-set-key (kbd "s-C-j") (lambda () (interactive) (split-window-below) (other-window 1)))
+(exwm-input-set-key (kbd "s-C-k") 'split-window-below)
+(exwm-input-set-key (kbd "s-C-l") (lambda () (interactive) (split-window-right) (other-window 1)))
+
+(push ?\s-\S-h exwm-input-prefix-keys)
+(push ?\s-\S-j exwm-input-prefix-keys)
+(push ?\s-\S-k exwm-input-prefix-keys)
+(push ?\s-\S-l exwm-input-prefix-keys)
+(exwm-input-set-key (kbd "s-S-l") 'evil-window-increase-width)
+(exwm-input-set-key (kbd "s-S-h") 'evil-window-decrease-width)
+(exwm-input-set-key (kbd "s-S-k") 'evil-window-increase-height)
+(exwm-input-set-key (kbd "s-S-j") 'evil-window-decrease-height)
+
+(push ?\: exwm-input-prefix-keys)
+
+
+
+
+(defvar exwm-app-launcher--prompt "$ "
+  "Prompt for the EXWM application launcher")
+(defun exwm-app-launcher (command)
+      (interactive (list (read-shell-command exwm-app-launcher--prompt)))
+      (start-process-shell-command command nil command))
+
+(defun exwm-rename-buffer ()
+  (interactive)
+  (exwm-workspace-rename-buffer
+   (concat exwm-class-name ":"
+           (if (<= (length exwm-title) 50) exwm-title
+             (concat (substring exwm-title 0 49) "...")))))
+
+;; Add these hooks in a suitable place (e.g., as done in exwm-config-default)
+(add-hook 'exwm-update-class-hook 'exwm-rename-buffer)
+(add-hook 'exwm-update-title-hook 'exwm-rename-buffer)
+
+
+(exwm-enable)
+
+
+
+(server-start)
+
 (require 'init-evil)
 (require 'init-theme)
 
@@ -241,7 +325,7 @@
     (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill)))
  '(package-selected-packages
    (quote
-    (notmuch elfeed writeroom-mode avy ag xref-js2 js2-refactor org-plus-contrib org-drill general evil-org org-bullets org-mode json-mode eyebrowse helm-swoop nethack nethack-el evil-paredit paredit racket-mode jade-mode js2-mode gnuplot gnuplot-mode elogcat smooth-scrolling telephone-line intero ranger hydra groovy-mode gradle-mode ace-jump-mode evil-snipe kotlin-mode helm-ag pyvenv evil-magit magit markdown-mode yaml-mode evil-surround url-http-extra-headers url-http smartparens haskell-mode buffer-move elscreen slack emacs-slack evil-leader helm-projectile projectile smart-mode-line nlinum clojure-mode evil)))
+    (exwm notmuch elfeed writeroom-mode avy ag xref-js2 js2-refactor org-plus-contrib org-drill general evil-org org-bullets org-mode json-mode eyebrowse helm-swoop nethack nethack-el evil-paredit paredit racket-mode jade-mode js2-mode gnuplot gnuplot-mode elogcat smooth-scrolling telephone-line intero ranger hydra groovy-mode gradle-mode ace-jump-mode evil-snipe kotlin-mode helm-ag pyvenv evil-magit magit markdown-mode yaml-mode evil-surround url-http-extra-headers url-http smartparens haskell-mode buffer-move elscreen slack emacs-slack evil-leader helm-projectile projectile smart-mode-line nlinum clojure-mode evil)))
  '(projectile-generic-command "find -L . -type f -print0")
  '(safe-local-variable-values (quote ((org-use-tag-inheritance))))
  '(telephone-line-mode t))
