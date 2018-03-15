c98a970133c7db4c930cb258e20fffda423c1ab1
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-03-15 07:31:35 +0100

    Add header and nicer fringes to emacs

--- .emacs.d/scripts/init-theme.el
+++ .emacs.d/scripts/init-theme.el
@@ -4,7 +4,7 @@
 (load-theme 'chip t)
 
 ;; margin stuff
-(set-frame-parameter nil 'internal-border-width 10)
+;;(set-frame-parameter nil 'internal-border-width 10)
 
 ;; disable gui fluff
 (scroll-bar-mode -1)
@@ -36,4 +36,115 @@
   (telephone-line-mode t))
 
 ;; set font
-(set-frame-font "-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*" nil t)
+(set-frame-font "-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*" t t)
+
+;; Change fringe icons
+(define-fringe-bitmap 'left-curly-arrow
+  [#b11011000
+   #b01101100
+   #b00110110
+   #b00011011
+   #b00110110
+   #b01101100
+   #b11011000])
+(define-fringe-bitmap 'right-curly-arrow
+  [#b00011011
+   #b00110110
+   #b01101100
+   #b11011000
+   #b01101100
+   #b00110110
+   #b00011011])
+
+
+
+(use-package all-the-icons
+  :ensure t)
+
+(defun chip/show-header ()
+  ""
+  (interactive)
+  (progn
+    (sl/display-header)
+    (set-face-attribute 'header-line nil :box '(:line-width 8 :color "#21242b"))
+    (set-face-background 'header-line "#21242b")
+    ;(set-face-background 'header-line "#282c34")
+    ))
+
+(defun chip/hide-header ()
+  ""
+  (interactive)
+  (setq header-line-format nil))
+
+(defun chip/update-header ()
+  ""
+  (interactive)
+  ;(if (buffer-file-name)
+  (if (eq major-mode 'exwm-mode)
+      (chip/hide-header)
+    (chip/show-header)))
+
+(defmacro with-face (str &rest properties)
+  `(propertize ,str 'face (list ,@properties)))
+
+(defun sl/make-header ()
+  ""
+  (let* ((sl/full-header (abbreviate-file-name buffer-file-name))
+         (sl/header (file-name-directory sl/full-header))
+         (sl/drop-str "[...]"))
+    (if (> (length sl/full-header)
+           (window-body-width))
+        (if (> (length sl/header)
+               (window-body-width))
+            (progn
+              (concat (with-face sl/drop-str
+                                 :background "blue"
+                                 :weight 'bold
+                                 )
+                      (with-face (substring sl/header
+                                            (+ (- (length sl/header)
+                                                  (window-body-width))
+                                               (length sl/drop-str))
+                                            (length sl/header))
+                                 ;; :background "red"
+                                 :weight 'bold
+                                 )))
+          (concat (with-face sl/header
+                             ;; :background "red"
+                             :foreground "#b9e59f"
+                             :weight 'bold
+                             )))
+      (concat (with-face sl/header
+                         ;; :background "green"
+                         ;; :foreground "black"
+                         :weight 'bold
+                         :foreground "#b9e59f"
+                         )
+              (with-face (file-name-nondirectory buffer-file-name)
+                         :weight 'bold
+                         ;; :background "red"
+                         )))))
+
+(defun sl/display-header ()
+  (setq header-line-format
+        '("" ;; invocation-name
+          (:eval (if (buffer-file-name)
+                     (chip/create-header-filepath)
+                     (chip/create-header-buffer))))))
+
+(defun chip/create-header-filepath ()
+  ;(concat (with-face "g" :background "#ff0000")
+  (concat " "
+          (concat (chip/header-icon)
+                  (concat " " (sl/make-header)))))
+
+(defun chip/create-header-buffer ()
+  (concat " "
+          (concat (chip/header-icon)
+                  " %b")))
+
+(defun chip/header-icon ()
+  (all-the-icons-faicon "codepen" :height 0.8 :v-adjust 0.1))
+
+(add-hook 'buffer-list-update-hook
+          'chip/update-header)
