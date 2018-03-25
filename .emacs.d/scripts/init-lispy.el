2b3ffb9c121498fb08cf1ea3cf107d237064eed7
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-03-25 08:54:33 +0200

    Switch lispyville to evil-lispy

--- .emacs.d/scripts/init-lispy.el
+++ .emacs.d/scripts/init-lispy.el
@@ -3,12 +3,21 @@
 (use-package lispy
   :ensure t
   :config
-  (add-hook 'emacs-lisp-mode-hook #'lispy-mode)
-  (add-hook 'clojure-mode-hook #'lispy-mode)
-  (add-hook 'clojurescript-mode-hook #'lispy-mode))
+  ;; (add-hook 'emacs-lisp-mode-hook #'lispy-mode)
+  ;; (add-hook 'clojure-mode-hook #'lispy-mode)
+  ;; (add-hook 'clojurescript-mode-hook #'lispy-mode)
+  )
 
-(use-package lispyville
+;; (use-package lispyville
+;;   :ensure t
+;;   :after lispy
+;;   :config
+;;   (add-hook 'lispy-mode-hook #'lispyville-mode))
+
+(use-package evil-lispy
   :ensure t
   :after lispy
   :config
-  (add-hook 'lispy-mode-hook #'lispyville-mode))
+  (add-hook 'emacs-lisp-mode-hook #'evil-lispy-mode)
+  (add-hook 'clojure-mode-hook #'evil-lispy-mode)
+  (add-hook 'clojurescript-mode-hook #'evil-lispy-mode))
