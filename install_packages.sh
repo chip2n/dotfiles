d27542f89a80b62d9266c71685109d208cecbf6a
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-03-31 12:20:20 +0200

    Add simple bash script for installing important arch packages

---
+++ install_packages.sh
@@ -0,0 +1,9 @@
+pacman -S --needed \
+	xorg-server xorg-xset xorg-xmodmap \   # x11
+	alsa-utils \                           # sound
+        xmonad xmonad-contrib haskell-dbus \   # wm
+	terminus-font \                        # gui
+        rxvt-unicode firefox vim emacs \       # apps
+	haskell-stack cabal-install \          # haskell
+	cmake python python2 wget unzip \      # misc
+
