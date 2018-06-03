7629963697248cd49a2abef977a90021924a83c5
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-03 16:24:52 +0200

    Diminish a bunch of modes

--- .emacs.d/scripts/init-diminish.el
+++ .emacs.d/scripts/init-diminish.el
@@ -2,10 +2,18 @@
 
 (use-package diminish
   :ensure t
-  :after (ivy projectile evil-snipe)
+  :after (ivy projectile evil-snipe evil-lispy)
   :config
   (diminish 'undo-tree-mode)
   (diminish 'company-mode)
   (diminish 'counsel-mode)
   (diminish 'ivy-mode)
-  (diminish 'projectile-mode))
+  (diminish 'projectile-mode)
+  (diminish 'projectile-mode)
+  (diminish 'evil-snipe-mode)
+  (diminish 'evil-snipe-local-mode)
+  (diminish 'lispy-mode)
+  (diminish 'evil-lispy-mode)
+  (diminish 'auto-revert-mode "auto-revert")
+  (diminish 'eldoc-mode)
+  (diminish 'yas-minor-mode))
