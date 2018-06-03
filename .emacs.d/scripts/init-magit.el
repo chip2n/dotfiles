a62bc8d58e6e58d08ffadcb8b61d8ae4e3cbcdf9
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-03 22:58:33 +0200

    Skip showing line numbers in magit buffer

--- .emacs.d/scripts/init-magit.el
+++ .emacs.d/scripts/init-magit.el
@@ -1,7 +1,9 @@
 (provide 'init-magit)
 
 (use-package magit
-  :ensure t)
+  :ensure t
+  :config
+  (add-hook 'magit-mode-hook (lambda () (display-line-numbers-mode -1))))
 (use-package evil-magit
   :ensure t
   :after magit)
