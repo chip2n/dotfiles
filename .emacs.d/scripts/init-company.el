fd3c558e49e040e0c0e7632f395fa372a230541b
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-26 20:57:09 +0200

    Add next/prev keybindings to company-mode

--- .emacs.d/scripts/init-company.el
+++ .emacs.d/scripts/init-company.el
@@ -3,10 +3,20 @@
 (use-package company
   :ensure t
   :config
-  (global-company-mode))
+  (global-company-mode)
+  (add-hook 'company-mode-hook 'chip/setup-company-keys))
 
 (use-package company-anaconda
   :ensure t
   :after (company anaconda-mode)
   :config
   (add-to-list 'company-backends 'company-anaconda))
+
+(defun chip/setup-company-keys ()
+  "Setup keybindings for company mode"
+  (interactive)
+  (general-define-key
+   :states 'insert
+   :keymaps 'company-mode-map
+   "C-j" 'company-select-next
+   "C-k" 'company-select-previous))
