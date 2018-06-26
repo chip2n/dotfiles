5b2ddb6bbafae85ecdb20da64460e8595b61ddef
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-26 20:44:20 +0200

    Fix ERC prompt and buffer scrolling

--- .emacs.d/scripts/init-erc.el
+++ .emacs.d/scripts/init-erc.el
@@ -2,4 +2,9 @@
 
 (require 'erc)
 
-(setq erc-prompt ">> ")
+(setq erc-prompt " ")
+(setq erc-fill-column 90)
+
+;; Keep emacs from recentering erc buffers
+(add-to-list 'erc-mode-hook (lambda ()
+                              (set (make-local-variable 'scroll-conservatively) 100)))
