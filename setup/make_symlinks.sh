3dbffbf6c649e6b947ecbd09caaf95fa95fe70c1
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-28 06:49:42 +0200

    Add more symlinks to make_symlinks.sh

--- setup/make_symlinks.sh
+++ setup/make_symlinks.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-ln -s ../.nethackrc /home/chip/.nethackrc
+ln -s /home/chip/dotfiles/.xinitrc /home/chip/.xinitrc
+ln -s /home/chip/dotfiles/.xmonad /home/chip/.xmonad
+ln -s /home/chip/dotfiles/.colors /home/chip/.colors
+ln -s /home/chip/dotfiles/.urxvt /home/chip/.urxvt
+ln -s /home/chip/dotfiles/.compton.conf /home/chip/.compton.conf
+ln -s /home/chip/dotfiles/.Xdefaults /home/chip/.Xdefaults
+ln -s /home/chip/dotfiles/.zshrc /home/chip/.zshrc
+ln -s /home/chip/dotfiles/.config /home/chip/.config
+ln -s /home/chip/dotfiles/scripts /home/chip/scripts
+ln -s /home/chip/dotfiles/.emacs.d /home/chip/.emacs.d
+ln -s /home/chip/dotfiles/.nethackrc /home/chip/.nethackrc
