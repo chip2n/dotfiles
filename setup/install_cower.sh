79b56ced42fac24022bdab7e50fde4d3b872d610
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-29 17:45:01 +0200

    Fix issue in cower installation script

--- setup/install_cower.sh
+++ setup/install_cower.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53          
+gpg --recv-key 1EB2638FF56C0C53
 mkdir cower
 cd cower
 wget -O PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
