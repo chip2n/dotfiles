2e810a3d643a76f3082fa32a8b4765244bf6499a
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-06-29 17:45:19 +0200

    Add .bashrc to make_symlinks.sh

--- setup/make_symlinks.sh
+++ setup/make_symlinks.sh
@@ -7,6 +7,7 @@ ln -s /home/chip/dotfiles/.urxvt /home/chip/.urxvt
 ln -s /home/chip/dotfiles/.compton.conf /home/chip/.compton.conf
 ln -s /home/chip/dotfiles/.Xdefaults /home/chip/.Xdefaults
 ln -s /home/chip/dotfiles/.zshrc /home/chip/.zshrc
+ln -s /home/chip/dotfiles/.bashrc /home/chip/.bashrc
 ln -s /home/chip/dotfiles/.config /home/chip/.config
 ln -s /home/chip/dotfiles/scripts /home/chip/scripts
 ln -s /home/chip/dotfiles/.emacs.d /home/chip/.emacs.d
