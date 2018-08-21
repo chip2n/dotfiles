55833c2929288d39e1edd905a3e66defcd68a83d
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-08-21 15:53:52 +0200

    Add some fancy features for dart

--- .emacs.d/scripts/init-dart.el
+++ .emacs.d/scripts/init-dart.el
@@ -1,4 +1,23 @@
 (provide 'init-dart)
 
 (use-package dart-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist (cons (rx ".dart" eos) 'dart-mode))
+  (setq dart-enable-analysis-server t)
+  (setq dart-sdk-path "/home/chip/flutter/bin/cache/dart-sdk/")
+  (add-hook 'dart-mode-hook 'flycheck-mode)
+  (add-hook 'dart-mode-hook 'chip/setup-dart-keys))
+
+(defun chip/setup-dart-keys ()
+  "Setup keybindings for dart mode"
+  (interactive)
+  (general-define-key
+   :states 'normal
+   :keymaps 'dart-mode-map
+   "gd" 'dart-goto))
+
+;; (require 'dart-mode)
+;; (add-to-list 'auto-mode-alist (cons (rx ".dart" eos) 'dart-mode))
+;; (setq dart-enable-analysis-server t)
+;; (setq dart-sdk-path "/home/chip/flutter/bin/cache/dart-sdk/")
