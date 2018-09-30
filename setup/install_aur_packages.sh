fbffc3381326f3238ee3a8dc4850f58ef5e9c743
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-09-30 12:29:22 +0200

    Add some AUR packages to install script

--- setup/install_aur_packages.sh
+++ setup/install_aur_packages.sh
@@ -3,3 +3,27 @@ cd dmenu2
 makepkg -ci
 cd ..
 rm -r dmenu2
+
+cower -d polybar
+cd polybar
+makepkg -ci
+cd ..
+rm -rf polybar
+
+cower -d zscroll-git
+cd zscroll-git
+makepkg -ci
+cd ..
+rm -rf zscroll-git
+
+cower -d siji-git
+cd siji-git
+makepkg -ci
+cd ..
+rm -rf siji-git
+
+cower -d dropbox
+cd dropbox
+makepkg -ci
+cd ..
+rm -rf dropbox
