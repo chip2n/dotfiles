2985167934654ad65e017d489748bef63d68ed49
Andreas Arvidsson <andreas.arvidson@gmail.com>
2018-11-13 21:26:27 +0100

    Add some packages to setup script

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -1,6 +1,6 @@
 pacman -Syyuu --needed \
   xorg-server xorg-xinit xorg-xset xorg-xmodmap xorg-xfontsel xorg-xrandr feh \
-  ttf-hack noto-fonts-emoji \
+  ttf-hack noto-fonts-emoji adobe-source-han-sans-jp-fonts \
   alsa-utils \
   xmonad xmonad-contrib haskell-dbus \
   i3-gaps i3blocks \
@@ -14,3 +14,5 @@ pacman -Syyuu --needed \
   playerctl \
   lib32-gcc-libs lib32-glibc lib32-zlib libxlst libnotify \
   cmake python python2 wget unzip openssh zlib libpng poppler-glib \
+  zathura zathura-pdf-poppler \
+  isync mu \
