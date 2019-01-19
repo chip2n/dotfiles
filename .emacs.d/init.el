1135b32f5de449dc54e24ba7932280844681b489
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-01-19 14:00:50 +0100

    Add function for incrementing numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -98,6 +98,14 @@
   :config
   (winner-mode 1))
 
+;; useful for macros
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
 ;; copy file name of current buffer
 (defun chip/copy-file-name-of-buffer ()
   "Put the current file name on the clipboard"
