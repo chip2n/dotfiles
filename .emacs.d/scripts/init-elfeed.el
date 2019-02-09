270331f00cc2d865c761fee6f2f021d4b7619935
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-02-09 08:52:30 +0100

    Add some more RSS feeds to elfeed

--- .emacs.d/scripts/init-elfeed.el
+++ .emacs.d/scripts/init-elfeed.el
@@ -1,10 +1,14 @@
 (provide 'init-elfeed)
 
 (use-package elfeed
-  :ensure t)
-
-(setq elfeed-feeds
-      '("http://feeds.feedburner.com/blogspot/hsDu"
-	"http://oremacs.com/atom.xml"
-	"http://pragmaticemacs.com/feed/"
-        ))
+  :ensure t
+  :config
+  (setq shr-inhibit-images t)           ;; disable image loading when viewing entries
+  (setq elfeed-feeds
+        '(("http://feeds.feedburner.com/blogspot/hsDu" android) ;; Android Developers Blog
+	  ("http://oremacs.com/atom.xml" emacs)                 ;; (or emacs)
+	  ("http://pragmaticemacs.com/feed/" emacs)             ;; Pragmatic Emacs
+          ("http://planet.emacsen.org/atom.xml" emacs)          ;; Planet Emacsen
+          ("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" news) ;; BBC News - Science & Environment
+          ("https://www.theverge.com/rss/index.xml" news) ;; The Verge
+          )))
