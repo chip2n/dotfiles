7c7709ba48c12cd9dc25fff7a609c8f3a6cc6ddc
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-02-11 08:08:38 +0100

    Add code for generating custom IDs in org-mode

--- .emacs.d/scripts/init-org.el
+++ .emacs.d/scripts/init-org.el
@@ -114,3 +114,30 @@
                  :category "remente")
           ))
   (org-super-agenda-mode))
+
+;; stolen from: https://writequit.org/articles/emacs-org-mode-generate-ids.html#automating-id-creation
+(require 'org-id)
+(setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
+
+(defun org-create-custom-id ()
+  (interactive)
+  (chip/org-custom-id-get (point) 'create))
+
+(defun chip/org-custom-id-get (&optional pom create prefix)
+  "Get the CUSTOM_ID property of the entry at point-or-marker POM.
+   If POM is nil, refer to the entry at point. If the entry does
+   not have an CUSTOM_ID, the function returns nil. However, when
+   CREATE is non nil, create a CUSTOM_ID if none is present
+   already. PREFIX will be passed through to `org-id-new'. In any
+   case, the CUSTOM_ID of the entry is returned."
+  (interactive)
+  (org-with-point-at pom
+    (let ((id (org-entry-get nil "CUSTOM_ID")))
+      (cond
+       ((and id (stringp id) (string-match "\\S-" id))
+        id)
+       (create
+        (setq id (org-id-new (concat prefix "h")))
+        (org-entry-put pom "CUSTOM_ID" id)
+        (org-id-add-location id (buffer-file-name (buffer-base-buffer)))
+        id)))))
