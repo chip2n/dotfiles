ab15cc114564944e562e46746c7e951542738096
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-06-19 18:58:01 +0200

    Add geiser config

--- .emacs.d/scripts/init-racket.el
+++ .emacs.d/scripts/init-racket.el
@@ -3,9 +3,10 @@
   :config
   (add-to-list 'auto-mode-alist (cons (rx ".rkt" eos) 'racket-mode)))
 
-;; (use-package geiser
-;;   :ensure t
-;;   :config
-;;   (add-to-list 'auto-mode-alist (cons (rx ".rkt" eos) 'geiser-mode)))
+(use-package geiser
+  :ensure t
+  :config
+  ;; (add-to-list 'auto-mode-alist (cons (rx ".rkt" eos) 'geiser-mode))
+  )
 
 (provide 'init-racket)
