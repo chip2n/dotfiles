eb32040c1f2f0a330a81659ff9b53870adf946a2
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-07-12 07:45:38 +0200

    Make google-translate insert translation in current buffer

--- .emacs.d/scripts/init-google-translate.el
+++ .emacs.d/scripts/init-google-translate.el
@@ -8,4 +8,6 @@
   ;; workaround for issue #52
   ;; https://github.com/atykhonov/google-translate/issues/52
   (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130))
-  )
+
+  ;; insert translations in current buffer
+  (setq google-translate-output-destination 'current-buffer))
