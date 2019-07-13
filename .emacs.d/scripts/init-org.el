4edf029aa0e0b350edef7ef2fd4f8ed3456ddd03
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-07-13 22:51:20 +0200

    Change look of agenda items

--- .emacs.d/scripts/init-org.el
+++ .emacs.d/scripts/init-org.el
@@ -76,6 +76,14 @@
 
 (advice-add 'org-agenda-finalize-entries :filter-return #'chip/org-agenda-finalize-entries)
 
+
+(defvar chip/org-agenda-scheduled-icon (all-the-icons-material "event" :v-adjust -0.1))
+(defvar chip/org-agenda-deadline-icon (all-the-icons-material "whatshot" :v-adjust -0.1))
+(setq org-agenda-scheduled-leaders `(,(concat chip/org-agenda-scheduled-icon "") ,(concat chip/org-agenda-scheduled-icon "x%1d")))
+(setq org-agenda-deadline-leaders `(,(concat chip/org-agenda-deadline-icon "")
+                                    ,(concat chip/org-agenda-deadline-icon "+%1d")
+                                    ,(concat chip/org-agenda-deadline-icon "-%1d")))
+
 ;; set org todo keywords
 (setq org-todo-keywords
       '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c@)")))
