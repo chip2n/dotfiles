ad5ff14ebaba89cd61cc80e4ea37d72a9ce67920
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-07-14 08:39:43 +0200

    Add tarsius blog to elfeed

--- .emacs.d/scripts/init-elfeed.el
+++ .emacs.d/scripts/init-elfeed.el
@@ -3,12 +3,13 @@
 (use-package elfeed
   :ensure t
   :config
-  (setq shr-inhibit-images t)           ;; disable image loading when viewing entries
+  (setq shr-inhibit-images t)           ; disable image loading when viewing entries
   (setq elfeed-feeds
-        '(("http://feeds.feedburner.com/blogspot/hsDu" android) ;; Android Developers Blog
-	  ("http://oremacs.com/atom.xml" emacs)                 ;; (or emacs)
-	  ("http://pragmaticemacs.com/feed/" emacs)             ;; Pragmatic Emacs
-          ("http://planet.emacsen.org/atom.xml" emacs)          ;; Planet Emacsen
-          ("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" news) ;; BBC News - Science & Environment
-          ("https://www.theverge.com/rss/index.xml" news) ;; The Verge
+        '(("http://feeds.feedburner.com/blogspot/hsDu" android) ; Android Developers Blog
+	  ("http://oremacs.com/atom.xml" emacs)                 ; (or emacs)
+	  ("http://pragmaticemacs.com/feed/" emacs)             ; Pragmatic Emacs
+          ("http://planet.emacsen.org/atom.xml" emacs)          ; Planet Emacsen
+          ("https://emacsair.me/feed.xml" emacs)                ; Tarsius blog
+          ("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" news) ; BBC News - Science & Environment
+          ("https://www.theverge.com/rss/index.xml" news) ; The Verge
           )))
