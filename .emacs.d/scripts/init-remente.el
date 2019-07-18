5f097f62b993a7b5926edfe3ff904f99b675953c
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-07-18 14:12:34 +0200

    Set correct paths for remente.el

--- .emacs.d/scripts/init-remente.el
+++ .emacs.d/scripts/init-remente.el
@@ -1,7 +1,9 @@
 (require 'remente nil t)
 
 (if (featurep 'remente)
-    (setq remente-path "/home/chip/git/remente-cli")
+    (progn
+      (setq remente-cli-path "/home/chip/git/remente-cli")
+      (setq remente-android-path "/home/chip/git/remente-android"))
   (warn "remente.el not in load-path"))
 
 (provide 'init-remente)
