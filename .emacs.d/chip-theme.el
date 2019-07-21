8342d47f0a9e847406008381fd06a09906a51fbb
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-07-21 11:36:27 +0200

    Tweak chiptheme

--- .emacs.d/chip-theme.el
+++ .emacs.d/chip-theme.el
@@ -1,10 +1,6 @@
 (deftheme chip
   "Face colors using the chip palette (dark background).")
 
-;; start adding vars from let* form below
-(setq chip/color-grey-4 "#6b7385")
-(setq chip/color-comment 'chip/color-grey-4)
-
 (let* ((class '((class color) (min-colors 89)))
       ;; Chip palette colors.
       (white-1 "#ffffff")
@@ -17,53 +13,52 @@
       (pink-1 "#dc8cac") (pink-2 "#ecb3c9")
       (magenta-1 "#dc8cac") (magenta-2 "#ecb3c9")
 
-      (highlight grey-2)
-      (diff-added "#556f55")
+      ;; high level definitions
+      (color-background    grey-1)
+      (color-foreground    white-1)
+      (color-comment       grey-4)
+      (color-cursor        grey-4)
+      (color-highlight-1   grey-2)
+      (color-highlight-2   grey-3)
+      (color-border        grey-2)
+      (color-headline-1    green-2)
+      (color-headline-2    blue-2)
+      (color-headline-3    pink-2)
+      (color-headline-4    red-2)
+      (color-link          blue-2)
+      (color-link-visited  blue-1)
+      (color-error         red-2)
+      (color-success       green-2)
+      (color-diff-added "#556f55")
 
-      ;; Tango palette colors.
-      (butter-1 "#fce94f") (butter-2 "#edd400") (butter-3 "#c4a000")
-      (orange-1 "#fcaf3e") (orange-2 "#f57900") (orange-3 "#ce5c00")
-      (choc-1 "#e9b96e") (choc-2 "#c17d11") (choc-3 "#8f5902")
-      (cham-1 "#8ae234") (cham-2 "#73d216") (cham-3 "#4e9a06")
-      (blue-3 "#204a87")
-      (plum-1 "#ad7fa8") (plum-2 "#75507b") (plum-3 "#5c3566")
-      (red-3 "#a40000")
-      (alum-1 "#eeeeec") (alum-2 "#d3d7cf") (alum-3 "#babdb6")
-      (alum-4 "#888a85") (alum-5 "#5f615c") (alum-6 "#2e3436")
-      ;; Not in Tango palette; used for better contrast.
-      (cham-4 "#346604") (blue-0 "#8cc4ff") (orange-4 "#b35000"))
+      )
 
-  (custom-theme-set-faces
-   'chip
-   `(default ((,class (:foreground ,white-1 :background ,grey-1))))
-   `(cursor ((,class (:background ,grey-4))))
+  (custom-theme-set-faces 'chip
+   `(default ((,class (:foreground ,color-foreground :background ,color-background))))
+   `(cursor ((,class (:background ,color-cursor))))
 
    ;; Highlighting faces
-   `(fringe ((,class (:foreground ,grey-3 :background ,grey-1))))
-   `(highlight ((,class (:background ,highlight))))
-   `(region ((,class (:background ,grey-3))))
+   `(fringe ((,class (:foreground ,color-comment :background ,color-background))))
+   `(highlight ((,class (:background ,color-highlight-1))))
+   `(region ((,class (:background ,color-highlight-2))))
    `(secondary-selection ((,class (:background ,grey-2))))
    `(isearch ((,class (:foreground ,white-1 :background ,blue-1))))
-   `(lazy-highlight ((,class (:background ,grey-3))))
-   `(trailing-whitespace ((,class (:background ,red-2))))
-   `(vertical-border ((,class (:foreground ,grey-2))))
+   `(lazy-highlight ((,class (:background ,color-highlight-2))))
+   `(trailing-whitespace ((,class (:background ,color-error))))
+   `(vertical-border ((,class (:foreground ,color-border))))
    `(link ((,class (:foreground ,blue-2 :underline t))))
    `(link-visited ((,class (:foreground ,blue-1 :underline t))))
-   `(line-number ((,class (:foreground ,grey-3))))
+   `(line-number ((,class (:foreground ,color-comment))))
 
    ;; Paren match
-   `(show-paren-match
-     ((,class (:background ,grey-4))))
+   `(show-paren-match ((,class (:background ,color-cursor))))
 
    ;; Header faces
-   `(header-line ((,class (:box ()
-			 :background ,grey-2 :foreground ,white-1))))
+   `(header-line ((,class (:box () :background ,color-background :foreground ,color-foreground))))
 
    ;; Mode line faces
-   `(mode-line ((,class (:box ()
-			 :background ,grey-2 :foreground ,white-1))))
-   `(mode-line-inactive ((,class (:box ()
-				  :background ,grey-2 :foreground ,grey-3))))
+   `(mode-line ((,class (:box () :background ,grey-2 :foreground ,white-1))))
+   `(mode-line-inactive ((,class (:box () :background ,grey-2 :foreground ,grey-3))))
 
    ;; Term colors
    `(term-color-black ((,class (:foreground ,grey-3))))
@@ -93,27 +88,27 @@
    `(org-ellipsis ((,class (:foreground ,grey-4))))
    `(org-todo ((,class (:foreground ,red-2 :weight bold))))
    `(org-done ((,class (:foreground ,green-2 :weight bold))))
-   `(org-level-1 ((,class (:foreground ,red-2 :weight bold))))
-   `(org-level-2 ((,class (:foreground ,blue-2 :weight bold))))
-   `(org-level-3 ((,class (:foreground ,pink-2 :weight bold))))
-   `(org-level-4 ((,class (:foreground ,green-2 :weight bold))))
-   `(org-level-5 ((,class (:foreground ,red-2 :weight bold))))
-   `(org-level-6 ((,class (:foreground ,blue-2 :weight bold))))
-   `(org-level-7 ((,class (:foreground ,pink-2 :weight bold))))
-   `(org-level-8 ((,class (:foreground ,green-2 :weight bold))))
-   `(org-link ((,class (:foreground ,blue-2 :underline t))))
+   `(org-level-1 ((,class (:foreground ,color-headline-1 :weight bold))))
+   `(org-level-2 ((,class (:foreground ,color-headline-2 :weight bold))))
+   `(org-level-3 ((,class (:foreground ,color-headline-3 :weight bold))))
+   `(org-level-4 ((,class (:foreground ,color-headline-4 :weight bold))))
+   `(org-level-5 ((,class (:foreground ,color-headline-1 :weight bold))))
+   `(org-level-6 ((,class (:foreground ,color-headline-2 :weight bold))))
+   `(org-level-7 ((,class (:foreground ,color-headline-3 :weight bold))))
+   `(org-level-8 ((,class (:foreground ,color-headline-4 :weight bold))))
+   `(org-link ((,class (:foreground ,color-link :underline t))))
    `(org-date ((,class (:foreground ,blue-2 :underline t))))
    `(org-tag ((,class (:foreground ,grey-3 :slant italic))))
    `(org-upcoming-deadline ((,class (:foreground ,grey-4))))
    `(org-upcoming-distant-deadline ((,class (:foreground ,grey-4))))
-   `(org-warning ((,class (:foreground ,red-2))))
+   `(org-warning ((,class (:foreground ,color-error))))
 
    ;; Org-agenda
    `(org-agenda-structure ((,class (:foreground ,grey-4))))
-   `(org-agenda-date ((,class (:foreground ,green-2 :weight bold))))
-   `(org-agenda-date-today ((,class (:foreground ,green-2 :weight bold :underline t))))
-   `(org-agenda-date-weekend ((,class (:foreground ,green-2 :weight bold))))
-   `(org-time-grid ((,class (:foreground ,grey-3))))
+   `(org-agenda-date ((,class (:foreground ,color-headline-1 :weight bold))))
+   `(org-agenda-date-today ((,class (:foreground ,color-headline-1 :weight bold :underline t))))
+   `(org-agenda-date-weekend ((,class (:foreground ,color-headline-1 :weight bold))))
+   `(org-time-grid ((,class (:foreground ,color-comment))))
    `(org-agenda-current-time ((,class (:foreground ,pink-2 :weight bold))))
    `(org-super-agenda-header ((,class (:foreground ,grey-3 :weight bold))))
    `(org-scheduled-previously ((,class (:foreground ,red-2))))
@@ -122,7 +117,7 @@
    `(org-deadline ((,class (:foreground ,white-1))))
 
    ;; Magit faces
-   `(magit-section-highlight ((,class (:background ,highlight))))
+   `(magit-section-highlight ((,class (:background ,color-highlight-1))))
    `(magit-section-heading ((,class (:foreground ,pink-2 :weight bold))))
    `(magit-hash ((,class (:foreground ,grey-4))))
    `(magit-dimmed ((,class (:foreground ,grey-4))))
@@ -130,8 +125,8 @@
    `(magit-log-author ((,class (:foreground ,red-1))))
    `(magit-log-date ((,class (:foreground ,grey-4))))
    `(magit-log-graph ((,class (:foreground ,grey-4))))
-   `(magit-diff-added ((,class (:background ,diff-added :foreground ,white-1))))
-   `(magit-diff-added-highlight ((,class (:background ,diff-added :foreground ,white-1))))
+   `(magit-diff-added ((,class (:background ,color-diff-added :foreground ,white-1))))
+   `(magit-diff-added-highlight ((,class (:background ,color-diff-added :foreground ,white-1))))
    `(magit-diff-hunk-heading ((,class (:background ,grey-2 :foreground ,grey-4))))
    `(magit-diff-hunk-heading-highlight ((,class (:background ,grey-3 :foreground ,white-1))))
    `(magit-diff-context ((,class (:foreground ,grey-3))))
@@ -139,10 +134,8 @@
    `(magit-head ((,class (:foreground ,blue-2 :weight bold))))
    `(magit-branch-local ((,class (:foreground ,blue-2 :weight bold))))
    `(magit-branch-remote ((,class (:foreground ,green-2 :weight bold))))
-   `(magit-branch-remote-head ((,class (:box (:line-width 1)
-					     :foreground ,green-2))))
-   `(magit-branch-current ((,class (:box (:line-width 1)
-					     :foreground ,blue-2))))
+   `(magit-branch-remote-head ((,class (:box (:line-width 1) :foreground ,green-2))))
+   `(magit-branch-current ((,class (:box (:line-width 1) :foreground ,blue-2))))
    `(magit-tag ((,class (:foreground ,yellow-1 :weight bold))))
    `(git-commit-summary ((,class (:foreground ,white-1))))
 
@@ -152,12 +145,16 @@
    `(message-header-to ((,class (:foreground ,grey-4 :weight bold))))
    `(message-header-other ((,class (:foreground ,pink-2))))
 
+   ;; Neotree
+   `(neo-dir-link-face ((,class (:foreground ,color-link :weight bold))))
+   `(neo-root-dir-face ((,class (:foreground ,color-headline-1 :weight bold))))
+
    ;; Lispy
    `(lispy-face-hint ((,class (:foreground ,grey-4 :weight bold))))
 
    ;; CIDER
-   `(cider-test-success-face ((,class (:foreground ,green-2 :weight bold))))
-   `(cider-test-failure-face ((,class (:background ,red-2 :foreground ,white-1 :weight bold))))
+   `(cider-test-success-face ((,class (:foreground ,color-success :weight bold))))
+   `(cider-test-failure-face ((,class (:background ,color-error   :foreground ,white-1 :weight bold))))
 
    ;; SLIME faces
    `(slime-repl-inputed-output-face ((,class (:foreground ,red-1))))
@@ -191,18 +188,18 @@
 
    ;; Escape and prompt faces
    `(minibuffer-prompt ((,class (:weight bold :foreground ,grey-3))))
-   `(escape-glyph ((,class (:foreground ,red-3))))
-   `(homoglyph ((,class (:foreground ,red-3))))
-   `(error ((,class (:foreground ,red-2))))
-   `(warning ((,class (:foreground ,orange-3))))
-   `(success ((,class (:foreground ,cham-3))))
+   `(escape-glyph ((,class (:foreground ,red-2))))
+   `(homoglyph ((,class (:foreground ,red-2))))
+   `(error ((,class (:foreground ,color-error))))
+   `(warning ((,class (:foreground ,color-error))))
+   `(success ((,class (:foreground ,color-success))))
 
    ;; Font lock faces
    `(font-lock-builtin-face ((,class (:foreground ,pink-2))))
-   `(font-lock-comment-face ((,class (:foreground ,grey-4))))
+   `(font-lock-comment-face ((,class (:foreground ,color-comment))))
    `(font-lock-constant-face ((,class (:foreground ,blue-2))))
    `(font-lock-function-name-face ((,class (:foreground ,pink-2))))
-   `(font-lock-keyword-face ((,class (:foreground ,blue-2))))
+   `(font-lock-keyword-face ((,class (:foreground ,blue-2 :weight bold))))
    `(font-lock-string-face ((,class (:foreground ,green-2))))
    `(font-lock-type-face ((,class (:foreground ,red-2))))
    `(font-lock-variable-name-face ((,class (:foreground ,pink-2))))
@@ -212,32 +209,28 @@
 
    ;; Telephone line faces
    `(telephone-line-evil-normal
-     ((,class (:background ,grey-2 :foreground ,white-1))))
+     ((,class (:background ,grey-2 :foreground ,color-foreground :weight bold))))
    `(telephone-line-evil-insert
-     ((,class (:background ,blue-2 :foreground ,white-1))))
+     ((,class (:background ,blue-2 :foreground ,color-foreground :weight bold))))
    `(telephone-line-evil-visual
-     ((,class (:background ,blue-2 :foreground ,white-1))))
+     ((,class (:background ,blue-2 :foreground ,color-foreground :weight bold))))
    `(telephone-line-evil-emacs
-     ((,class (:background ,pink-1 :foreground ,white-1))))
+     ((,class (:background ,pink-1 :foreground ,color-foreground :weight bold))))
    `(telephone-line-evil-motion
-     ((,class (:background ,pink-1 :foreground ,white-1))))
+     ((,class (:background ,pink-1 :foreground ,color-foreground :weight bold))))
    `(telephone-line-accent-active
-     ((,class (:background ,grey-3 :foreground ,white-1))))
+     ((,class (:background ,grey-3 :foreground ,color-foreground))))
    `(telephone-line-accent-inactive
      ((,class (:background ,grey-2 :foreground ,grey-3))))
 
-   ;; Button and link faces
-   `(link ((,class (:underline t :foreground ,blue-3))))
-   `(link-visited ((,class (:underline t :foreground ,blue-2))))
-
    ;; Message faces
-   `(message-header-name ((,class (:foreground ,blue-3))))
-   `(message-header-cc ((,class (:foreground ,butter-3))))
-   `(message-header-other ((,class (:foreground ,choc-2))))
-   `(message-header-subject ((,class (:foreground ,red-3))))
-   `(message-header-to ((,class (:weight bold :foreground ,butter-3))))
-   `(message-cited-text ((,class (:slant italic :foreground ,alum-5))))
-   `(message-separator ((,class (:weight bold :foreground ,cham-3)))))
+   `(message-header-name ((,class (:foreground ,blue-2))))
+   `(message-header-cc ((,class (:foreground ,yellow-1))))
+   `(message-header-other ((,class (:foreground ,red-2))))
+   `(message-header-subject ((,class (:foreground ,red-2))))
+   `(message-header-to ((,class (:weight bold :foreground ,yellow-1))))
+   `(message-cited-text ((,class (:slant italic :foreground ,grey-4))))
+   `(message-separator ((,class (:weight bold :foreground ,green-2)))))
 
    ;; Tabbar faces
    `(tabbar-default
@@ -246,7 +239,7 @@
 
   (custom-theme-set-variables
    'chip
-   `(ansi-color-names-vector [,alum-6 ,red-3 ,cham-3 ,butter-3
-				      ,blue-3 ,plum-3 ,blue-1 ,alum-1])))
+   `(ansi-color-names-vector [,grey-3 ,red-2 ,green-2 ,yellow-1
+				      ,blue-2 ,magenta-2 ,blue-1 ,grey-4])))
 
 (provide-theme 'chip)
