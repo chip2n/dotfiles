d0def8c4a8dc9b895c15713fc505e470ec00e634
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-07-28 08:55:57 +0200

    Tweak elfeed rss stream list

--- .emacs.d/scripts/init-elfeed.el
+++ .emacs.d/scripts/init-elfeed.el
@@ -8,8 +8,8 @@
         '(("http://feeds.feedburner.com/blogspot/hsDu" android) ; Android Developers Blog
 	  ("http://oremacs.com/atom.xml" emacs)                 ; (or emacs)
 	  ("http://pragmaticemacs.com/feed/" emacs)             ; Pragmatic Emacs
-          ("http://planet.emacsen.org/atom.xml" emacs)          ; Planet Emacsen
           ("https://emacsair.me/feed.xml" emacs)                ; Tarsius blog
           ("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" news) ; BBC News - Science & Environment
           ("https://www.theverge.com/rss/index.xml" news) ; The Verge
-          )))
+          ("https://defn.io/index.xml" racket)
+          "http://techsnuffle.com/feed.xml")))
