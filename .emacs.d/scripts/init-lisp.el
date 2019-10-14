7c1e6af2d7a00eff6ffd3640b939a5aa5348d41d
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-10-14 08:25:48 +0200

    Add slime-company

--- .emacs.d/scripts/init-lisp.el
+++ .emacs.d/scripts/init-lisp.el
@@ -4,6 +4,10 @@
   :ensure t
   :config
   (setq inferior-lisp-program "/usr/bin/sbcl")
-  (setq slime-contribs '(slime-fancy slime-asdf slime-cl-indent))
   (add-hook 'slime-repl-mode-hook 'header-mode)
-  (add-hook 'slime-macroexpansion-minor-mode-hook (lambda () (interactive) (evil-motion-state))))
+  (add-hook 'slime-macroexpansion-minor-mode-hook (lambda () (interactive) (evil-motion-state)))
+  (slime-setup '(slime-fancy slime-asdf slime-cl-indent slime-company)))
+
+(use-package slime-company
+  :ensure t
+  :after (slime company))
