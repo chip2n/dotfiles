7c86b17ff45399d3c9162629b8ac516b5151cfce
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-10-17 07:32:09 +0200

    Exclude untracked files in projectile search

--- .emacs.d/scripts/init-projectile.el
+++ .emacs.d/scripts/init-projectile.el
@@ -6,6 +6,10 @@
   :config
   (add-to-list 'projectile-globally-ignored-directories "*node_modules")
   (setq projectile-enable-caching t)
+  ;; we remove -o flag so that untracked files are not included
+  ;; this is mainly so that they don't always appear as the first search
+  (setq projectile-git-command "git ls-files -zc --exclude-standard")
+  (setq projectile-indexing-method 'alien)
   (setq projectile-completion-system 'ivy)
   (projectile-register-project-type 'shadow-cljs '("shadow-cljs.edn")
                                     :src-dir "src/main"
