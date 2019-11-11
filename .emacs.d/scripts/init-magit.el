422d58c9ba736d4224f2cf3ec46ad6f573d3e323
Andreas Arvidsson <andreas.arvidson@gmail.com>
2019-11-11 16:41:27 +0100

    Disable evil-magit

--- .emacs.d/scripts/init-magit.el
+++ .emacs.d/scripts/init-magit.el
@@ -6,9 +6,9 @@
   (add-hook 'magit-mode-hook (lambda () (display-line-numbers-mode -1)))
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
-(use-package evil-magit
-  :ensure t
-  :after magit)
+;; (use-package evil-magit
+;;   :ensure t
+;;   :after magit)
 
 (use-package forge
   :ensure t
