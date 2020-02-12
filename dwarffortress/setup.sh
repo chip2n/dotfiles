b6014ee5ba56f12f7d091e8f4e0d8854fa02a696
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-02-12 10:24:40 +0100

    Fix typo in DF setup script

--- dwarffortress/setup.sh
+++ dwarffortress/setup.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ln -sf /home/chip/dotfiles/dwarffortress/colors.txt /home/chip/.dwarffortress/data/init/colors.tx
+ln -sf /home/chip/dotfiles/dwarffortress/colors.txt /home/chip/.dwarffortress/data/init/colors.txt
 ln -sf /home/chip/Dropbox/dwarffortress/save /home/chip/.dwarffortress/data/
