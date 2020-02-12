bed857f66b35f6629b656d1ca388ef65c38fdb48
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-02-12 18:13:19 +0100

    Add Dwarf Fortress init.txt file

--- dwarffortress/setup.sh
+++ dwarffortress/setup.sh
@@ -3,4 +3,5 @@
 set -e
 
 ln -sf /home/chip/dotfiles/dwarffortress/colors.txt /home/chip/.dwarffortress/data/init/colors.txt
+ln -sf /home/chip/Dropbox/dwarffortress/init.txt /home/chip/.dwarffortress/data/init/init.txt
 ln -sf /home/chip/Dropbox/dwarffortress/save /home/chip/.dwarffortress/data/
