8e805bfc46304253e7caae183f1548c74bb1e1d7
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-02-22 09:48:59 +0100

    Add better dired sorting

--- .emacs.d/scripts/init-dired.el
+++ .emacs.d/scripts/init-dired.el
@@ -3,4 +3,7 @@
 ;; load dired-x immediately to make keybindings available
 (require 'dired-x)
 
+;; show directories before files
+(setq dired-listing-switches "-aBhl  --group-directories-first")
+
 (provide 'init-dired)
