5354e52da722087d2260fb7fe7cae0305edbe726
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-02-27 16:11:57 +0100

    Add Mickel's blog to elfeed feeds

--- .emacs.d/scripts/init-elfeed.el
+++ .emacs.d/scripts/init-elfeed.el
@@ -12,4 +12,5 @@
           ("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" news) ; BBC News - Science & Environment
           ("https://www.theverge.com/rss/index.xml" news) ; The Verge
           ("https://defn.io/index.xml" racket)
-          "http://techsnuffle.com/feed.xml")))
+          "http://techsnuffle.com/feed.xml"
+          "https://ultimatemachine.se/articles.xml")))
