474bdc2ae19134c7fae7aba4126ea9a07ddd8c05
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-03-01 13:28:15 +0100

    Clean up rust config

--- .emacs.d/scripts/init-rust.el
+++ .emacs.d/scripts/init-rust.el
@@ -19,32 +19,12 @@
   (general-define-key
    :states 'normal
    :keymaps 'rust-mode-map
-   "gd" 'racer-find-definition)
-  ;; (general-define-key
-  ;;  :prefix leader
-  ;;  :states 'normal
-  ;;  :mode 'cargo-minor-mode
-  ;;  "mr" 'cargo-process-run
-  ;;  "mc" 'cargo-process-check
-  ;;  "mt" 'cargo-process-test)
-  )
-
-;; (use-package flymake-rust
-;;   :ensure t
-;;   :after rust-mode)
+   "gd" 'racer-find-definition))
 
 (use-package flycheck-rust
   :ensure t
   :after rust-mode
   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-
-;; (use-package racer
-;;   :ensure t
-;;   :after rust-mode
-;;   :config
-;;   (add-hook 'rust-mode-hook #'racer-mode)
-;;   (add-hook 'racer-mode-hook #'eldoc-mode)
-;;   (add-hook 'racer-mode-hook #'company-mode))
 
 (use-package cargo
   :ensure t
