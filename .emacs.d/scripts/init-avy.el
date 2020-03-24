d7403b7aeaefa14e361f592ac98994a05f561d12
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-03-24 20:34:52 +0100

    Add ace-window as ivy action for ivy-switch-buffer

--- .emacs.d/scripts/init-avy.el
+++ .emacs.d/scripts/init-avy.el
@@ -6,4 +6,7 @@
 (use-package ace-window
   :ensure t
   :config
-  (setq aw-dispatch-always t))
+  (setq aw-dispatch-always t)
+  (ivy-add-actions
+   'ivy-switch-buffer
+   '(("a" ace-window "ace-window"))))
