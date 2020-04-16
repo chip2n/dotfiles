66c4fe7d53777f53bf173b9dee784386a1988c2f
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-04-16 15:28:23 +0200

    Add disabled config for sly

--- .emacs.d/scripts/init-lisp.el
+++ .emacs.d/scripts/init-lisp.el
@@ -16,3 +16,20 @@
 (defun slime-enable-concurrent-hints ()
   (interactive)
   (setf slime-inhibit-pipelining nil))
+
+(defun sly-mrepl-other-window ()
+  (interactive)
+  (sly-mrepl #'switch-to-buffer-other-window))
+
+;; (use-package sly
+;;   :ensure t
+;;   :after (company)
+;;   :config
+;;   (sly-setup '(;; sly-indentation
+;;                ))
+;;   (add-to-list 'evil-emacs-state-modes 'sly-db-mode)
+;;   (setq inferior-lisp-program "/usr/bin/sbcl")
+;;   (add-hook 'sly-mode-hook 'company-mode)
+;;   (general-define-key
+;;    :keymaps 'sly-mode-map
+;;    "C-c C-z" #'sly-mrepl-other-window))
