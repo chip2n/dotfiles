0695cd906e8ebfd08dbeaa823f4e6045abc8a0a8
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-05-09 09:32:11 +0200

    Add elixir packages

--- .emacs.d/scripts/init-elixir.el
+++ .emacs.d/scripts/init-elixir.el
@@ -8,4 +8,16 @@
   (add-hook 'lsp-after-initialize-hook
             (lambda () (lsp--set-configuration `(:elixirLS ,lsp-elixir--config-options)))))
 
+(use-package exunit
+  :ensure t)
+
+(use-package inf-elixir
+  :load-path "packages/inf-elixir/"
+  :bind (
+    ("C-c C-l i i" . 'inf-elixir)
+    ("C-c C-l i p" . 'inf-elixir-project)
+    ("C-c C-l i l" . 'inf-elixir-send-line)
+    ("C-c C-l i r" . 'inf-elixir-send-region)
+    ("C-c C-l i b" . 'inf-elixir-send-buffer)))
+
 (provide 'init-elixir)
