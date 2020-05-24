4ba19c0fcc920ab2b0ce8636fa21f978ace404f4
Andreas Arvidsson <andreas.arvidson@gmail.com>
2020-05-24 13:06:02 +0200

    Add fonts to setup script

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -1,6 +1,6 @@
 pacman -Syyuu --needed \
   xorg-server xorg-xinit xorg-xset xorg-xmodmap xorg-xfontsel xorg-xrandr xorg-xwininfo xorg-xprop xclip xdotool feh \
-  ttf-hack noto-fonts-emoji adobe-source-han-sans-jp-fonts adobe-source-han-sans-otc-fonts \
+  ttf-hack noto-fonts noto-fonts-emoji adobe-source-han-sans-jp-fonts adobe-source-han-sans-otc-fonts \
   alsa-utils \
   xmonad xmonad-contrib haskell-dbus \
   i3-gaps i3blocks \
