deb8e798375714225be8e522d2dd1b157a27e97c
chip2n <andreas@arvidsson.io>
2020-12-11 18:29:11 +0100

    Delete unused setup scripts

--- setup/install_cower.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gpg --recv-key 1EB2638FF56C0C53
-mkdir cower
-cd cower
-wget -O PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
-makepkg -ci
-cd ..
--- setup/install_scrcpy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pacman -S --needed android-tools
-
-cd /tmp
-auracle clone scrcpy
-cd scrcpy
-makepkg -ci
