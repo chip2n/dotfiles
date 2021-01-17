20d99d7144cde9eca9eb5f6db2b5abc576424801
chip2n <andreas@arvidsson.io>
2021-01-17 09:44:38 +0100

    Add decrement-number-at-point function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2670,6 +2670,13 @@ all elements."
       (error "No number at point"))
   (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
 
+(defun decrement-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1- (string-to-number (match-string 0))))))
+
 (defun date->unix ()
   (interactive)
   (let ((d (org-read-date)))
