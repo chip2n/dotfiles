1ac58ad312e898279d2f1a84ca54633905b6352c
chip2n <andreas@arvidsson.io>
2021-01-22 19:47:15 +0100

    Update android init.el

--- .emacs.d/new/chip-init-android.el
+++ .emacs.d/new/chip-init-android.el
@@ -22,22 +22,6 @@
 
 ;;; Code:
 
-(defvar bootstrap-version)
-
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(straight-use-package 'use-package)
-
-(setq straight-use-package-by-default t)
+(provide 'chip-init-android)
 
 ;;; chip-init-android.el ends here
