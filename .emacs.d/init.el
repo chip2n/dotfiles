6df5dfb7c9491372137e323ea2d0ab2f28962ccc
chip2n <andreas@arvidsson.io>
2021-01-22 21:42:28 +0100

    Add solaire-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,10 @@
 
 ;;; Code:
 
+;; (eval-when-compile
+;;   (add-to-list 'load-path "use-package")
+;;   (require 'use-package))
+
 (defvar bootstrap-version)
 
 (let ((bootstrap-file
@@ -35,6 +39,9 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 ;;; Source paths
 
@@ -50,8 +57,26 @@
   (native-compile-async "~/.emacs.d" 'recursively))
 
 ;;; Theme
+(use-package solaire-mode
+  ;; Ensure solaire-mode is running in all solaire-mode buffers
+  :hook (change-major-mode . turn-on-solaire-mode)
+  ;; ...if you use auto-revert-mode, this prevents solaire-mode from turning
+  ;; itself off every time Emacs reverts the file
+  :hook (after-revert . turn-on-solaire-mode)
+  ;; To enable solaire-mode unconditionally for certain modes:
+  :hook (ediff-prepare-buffer . solaire-mode)
+  ;; Highlight the minibuffer when it is activated:
+  :hook (minibuffer-setup . solaire-mode-in-minibuffer)
+  :config
+  ;; The bright and dark background colors are automatically swapped the first 
+  ;; time solaire-mode is activated. Namely, the backgrounds of the `default` and
+  ;; `solaire-default-face` faces are swapped. This is done because the colors 
+  ;; are usually the wrong way around. If you don't want this, you can disable it:
+  (setq solaire-mode-auto-swap-bg nil)
 
-(require 'chip-theme-ui)
+  (solaire-global-mode +1))
+
+;; (require 'chip-theme-ui)
 
 ;;; Desktop initialization
 
