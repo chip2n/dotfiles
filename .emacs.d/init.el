9dfa02acb34bd65ec92affa5f6e06dddb26e2ba5
chip2n <andreas@arvidsson.io>
2021-01-23 08:43:04 +0100

    Move org config to separate file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,7 +58,7 @@
 
 ;;; Desktop initialization
 
-;; (require 'chip-init-desktop)
-(require 'chip-init-android)
+(require 'chip-init-desktop)
+;; (require 'chip-init-android)
 
 ;;; init.el ends here
