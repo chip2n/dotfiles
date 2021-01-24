18fdedeb41b9624a2e4779ba806f1a372a576af2
chip2n <andreas@arvidsson.io>
2021-01-24 15:18:40 +0100

    Disable header mode in slime config

--- .emacs.d/src/chip-lang.el
+++ .emacs.d/src/chip-lang.el
@@ -4,7 +4,7 @@
   :config
   (setq inferior-lisp-program "/usr/bin/sbcl")
   (setq slime-description-autofocus t)
-  (add-hook 'slime-repl-mode-hook 'header-mode)
+  ;; (add-hook 'slime-repl-mode-hook 'header-mode)
   (add-hook 'slime-macroexpansion-minor-mode-hook (lambda () (interactive) (evil-motion-state)))
   (slime-setup '(slime-fancy slime-asdf slime-cl-indent slime-company slime-fuzzy))
 
