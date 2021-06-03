5b3f4a89c7c32b52694f271d37e8b3dc5e752cb0
chip2n <andreas@arvidsson.io>
2021-06-03 08:48:38 +0200

    Enable c/code-mode in zig buffers

--- .emacs.d/src/chip-init-desktop.el
+++ .emacs.d/src/chip-init-desktop.el
@@ -1241,6 +1241,7 @@ all elements."
   ;; see https://github.com/ziglang/zig-mode/issues/49
   (setq zig-format-on-save nil)
   (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
+  (add-hook 'zig-mode-hook 'c/code-mode)
   (add-hook 'zig-mode-hook 'lsp)
   (lsp-register-client
    (make-lsp-client
