f257ebb7c22568c1e6024b87e4724ddf98c484ea
chip2n <andreas@arvidsson.io>
2022-01-11 07:34:49 +0100

    Tweak zig config

--- .emacs.d/src/chip-code-zig.el
+++ .emacs.d/src/chip-code-zig.el
@@ -46,45 +46,52 @@
 (use-package zig-mode
   :after (lsp-mode)
   :bind (:map zig-mode-map
-              ("C-c C-r" . chip/zig-compile-run)
-              ("C-c C-b" . c/zig-build)
-              ("C-c C-k" . kill-compilation)
-              ("C-c C-t" . chip/zig-test)
-              ("C-c C-d" . chip/zig-test-this))
+         ("C-c C-r" . chip/zig-compile-run)
+         ("C-c C-b" . c/zig-build)
+         ("C-c C-k" . kill-compilation)
+         ("C-c C-t" . chip/zig-test)
+         ("C-c C-d" . chip/zig-test-this))
   :config
   ;; formatting on save breaks lsp-mode
   ;; see https://github.com/ziglang/zig-mode/issues/49
   (setq zig-format-on-save nil)
   (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
   (add-hook 'zig-mode-hook 'lsp)
+
+  (after-load (outshine)
+    (add-hook 'zig-mode-hook 'outshine-mode))
+
   (lsp-register-client
    (make-lsp-client
     :new-connection (lsp-stdio-connection "/home/chip/Downloads/x86_64-linux/zls")
     :major-modes '(zig-mode)
     :server-id 'zls)))
 
+(defun chip/zig--locate-root ()
+  (locate-dominating-file default-directory "build.zig"))
+
 (defun chip/zig-compile ()
   "Compile using `zig build`."
   (interactive)
-  (let ((default-directory (projectile-project-root)))
+  (let ((default-directory (chip/zig--locate-root)))
     (zig--run-cmd c/zig-build-cmd)))
 
 (defun chip/zig-compile-run ()
   "Compile and run using `zig build run`."
   (interactive)
-  (let ((default-directory (projectile-project-root)))
+  (let ((default-directory (chip/zig--locate-root)))
     (zig--run-cmd "build run")))
 
 (defun chip/zig-test ()
   "Test using `zig build test`."
   (interactive)
-  (let ((default-directory (projectile-project-root)))
+  (let ((default-directory (chip/zig--locate-root)))
     (zig--run-cmd "build test")))
 
 (defun chip/zig-test-this ()
   "Test current file using `zig test`."
   (interactive)
-  (let ((default-directory (projectile-project-root))
+  (let ((default-directory (chip/zig--locate-root))
         (path (buffer-file-name)))
     (zig--run-cmd "test" path)))
 
