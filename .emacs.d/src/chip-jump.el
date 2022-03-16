65ace7cbd7bb92015543f3899e74383e93780aaa
chip2n <andreas@arvidsson.io>
2022-03-16 06:04:00 +0100

    Update gf2 config

---
+++ .emacs.d/src/chip-jump.el
@@ -0,0 +1,40 @@
+;;; chip-jump.el -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021  Andreas Arvidsson
+;;
+;; Author: Andreas Arvidsson <andreas@arvidsson.io>
+;; Keywords: config
+;;
+;; This file is not part of GNU Emacs
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file contains code for jumping around code, buffers and whatnot.
+
+;;; Code:
+
+(use-package better-jumper
+  :bind (("C-o" . better-jumper-jump-backward)
+         ("C-S-o" . better-jumper-jump-forward))
+  :config
+  (with-eval-after-load 'evil-maps
+    (define-key evil-motion-state-map (kbd "C-o") 'better-jumper-jump-backward)
+    ;; (define-key evil-motion-state-map (kbd "<C-i>") 'better-jumper-jump-forward)
+    ))
+
+(provide 'chip-jump)
+
+;;; chip-jump.el ends here
