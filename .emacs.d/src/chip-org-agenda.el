23e3982e53fb8cfc7e7fa0a0a37697a7fc027c07
chip2n <andreas@arvidsson.io>
2022-10-03 14:40:24 +0200

    Hide refile tags in agenda

--- .emacs.d/src/chip-org-agenda.el
+++ .emacs.d/src/chip-org-agenda.el
@@ -156,6 +156,9 @@
 ;; Hide separators between agenda blocks
 (setq org-agenda-block-separator nil)
 
+;; Hide refile tags (shown in separate section anyways
+(setq org-agenda-hide-tags-regexp "refile")
+
 ;; Remove indentation from agenda blocks
 (setq org-agenda-prefix-format
       '((agenda . "%i%-12:c%?-12t% s")
