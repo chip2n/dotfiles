7c86a22f7b51124c091abcb01c3c105ecc5075fe
chip2n <andreas@arvidsson.io>
2022-11-14 10:20:20 +0100

    Refactor symlink script

--- .zshrc
+++ .zshrc
@@ -35,3 +35,7 @@ alias cl='clear'
 alias woman,='sudo'
 alias feh='feh --scale-down'
 alias vimr='gvim /home/chip/.vim/vimrc'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- setup/make_symlinks.sh
+++ setup/make_symlinks.sh
@@ -1,27 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-ln -sf /home/chip/dotfiles/.xinitrc /home/chip/.xinitrc
-ln -sf /home/chip/dotfiles/.xmodmap /home/chip/.xmodmap
-ln -sf /home/chip/dotfiles/.xmonad /home/chip/.xmonad
-ln -sf /home/chip/dotfiles/.colors /home/chip/.colors
-ln -sf /home/chip/dotfiles/.urxvt /home/chip/.urxvt
-ln -sf /home/chip/dotfiles/.compton.conf /home/chip/.compton.conf
-ln -sf /home/chip/dotfiles/.Xdefaults /home/chip/.Xdefaults
-ln -sf /home/chip/dotfiles/.zshrc /home/chip/.zshrc
-ln -sf /home/chip/dotfiles/.bashrc /home/chip/.bashrc
-ln -sf /home/chip/dotfiles/.config /home/chip/.config
-ln -sf /home/chip/dotfiles/scripts /home/chip/scripts
-ln -sf /home/chip/dotfiles/.emacs.d /home/chip/.emacs.d
-ln -sf /home/chip/dotfiles/.nethackrc /home/chip/.nethackrc
-ln -sf /home/chip/dotfiles/.mbsyncrc /home/chip/.mbsyncrc
-ln -sf /home/chip/dotfiles/.sbclrc /home/chip/.sbclrc
-ln -sf /home/chip/dotfiles/audio /home/chip/audio
-ln -sf /home/chip/Dropbox/elfeed /home/chip/.elfeed
-ln -sf /home/chip/dotfiles/.ideavimrc /home/chip/.ideavimrc
-ln -sf /home/chip/dotfiles/.asoundrc /home/chip/.asoundrc
-ln -sf /home/chip/dotfiles/.slynkrc /home/chip/.slynkrc
-ln -sf /home/chip/dotfiles/git_template /home/chip/.git_template
+DOTFILES=$HOME/dev/dotfiles
+
+ln -sf $DOTFILES/.asoundrc $HOME/.asoundrc
+ln -sf $DOTFILES/.bashrc $HOME/.bashrc
+ln -sf $DOTFILES/.colors $HOME/.colors
+ln -sf $DOTFILES/.compton.conf $HOME/.compton.conf
+ln -sf $DOTFILES/.config $HOME/.config
+ln -sf $DOTFILES/.emacs.d $HOME/.emacs.d
+ln -sf $DOTFILES/.ideavimrc $HOME/.ideavimrc
+ln -sf $DOTFILES/.mbsyncrc $HOME/.mbsyncrc
+ln -sf $DOTFILES/.nethackrc $HOME/.nethackrc
+ln -sf $DOTFILES/.sbclrc $HOME/.sbclrc
+ln -sf $DOTFILES/.slynkrc $HOME/.slynkrc
+ln -sf $DOTFILES/.urxvt $HOME/.urxvt
+ln -sf $DOTFILES/.Xdefaults $HOME/.Xdefaults
+ln -sf $DOTFILES/.xinitrc $HOME/.xinitrc
+ln -sf $DOTFILES/.xmodmap $HOME/.xmodmap
+ln -sf $DOTFILES/.xmonad $HOME/.xmonad
+ln -sf $DOTFILES/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES/audio $HOME/audio
+ln -sf $DOTFILES/git_template $HOME/.git_template
+ln -sf $DOTFILES/scripts $HOME/scripts
+
+ln -sf $HOME/storage/elfeed $HOME/.elfeed
