15b908c87d9135926abc9279aeb05129cb613185
chip2n <andreas@arvidsson.io>
2024-02-05 06:53:11 +0100

    Fix broken emacs config

--- .emacs.d/src/chip-init-desktop.el
+++ .emacs.d/src/chip-init-desktop.el
@@ -278,7 +278,7 @@ The default is to leave the cursor where it is, which is not as useful when sear
 ;; Allow C-w, M-w etc operate on current line if no region active
 (use-package whole-line-or-region
   :config
-  (whole-line-or))
+  (whole-line-or-region-global-mode))
 
 ;;; File navigation
 
