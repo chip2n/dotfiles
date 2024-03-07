247eeb90bc56459dbf89d0e478f61287e306c3f7
chip2n <andreas@arvidsson.io>
2024-03-07 20:38:00 +0100

    Update elixir config

--- .emacs.d/src/chip-code-elixir2.el
+++ .emacs.d/src/chip-code-elixir2.el
@@ -1,10 +1,47 @@
-(require 'heex-ts-mode)
-(require 'elixir-ts-mode)
+;;; chip-code-elixir.el -*- lexical-binding: t -*-
 
-(setq lsp-clients-elixir-server-executable '("~/dev/elixir-ls/release/language_server.sh"))
-(add-hook 'elixir-ts-mode-hook 'lsp)
-(add-hook 'elixir-ts-mode-hook 'electric-pair-local-mode)
-(add-to-list 'auto-mode-alist '("\\.heex\\'" . elixir-ts-mode))
+;; Copyright (C) 2022  Andreas Arvidsson
+;;
+;; Author: Andreas Arvidsson <andreas@arvidsson.io>
+;; Keywords: config
+;;
+;; This file is not part of GNU Emacs
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'eglot)
+
+(use-package elixir-ts-mode
+  :bind ("C-c C-f" . eglot-format-buffer)
+  :hook (elixir-ts-mode . eglot-ensure)
+  )
+
+;; ;; This is optional. It automatically runs `M-x eglot` for you whenever you are in `elixir-mode`:
+;; (add-hook 'elixir-mode-hook 'eglot-ensure)
+
+;; Be sure to edit the path appropriately; use the `.bat` script instead for Windows:
+(add-to-list 'eglot-server-programs '(elixir-ts-mode "/home/chip/dev/elixir-ls/release/language_server.sh"))
+
+;; (require 'heex-ts-mode)
+;; (require 'elixir-ts-mode)
+
+;; (setq lsp-clients-elixir-server-executable '("~/dev/elixir-ls/release/language_server.sh"))
+;; (add-hook 'elixir-ts-mode-hook 'lsp)
+;; (add-hook 'elixir-ts-mode-hook 'electric-pair-local-mode)
+;; (add-to-list 'auto-mode-alist '("\\.heex\\'" . elixir-ts-mode))
 
 (define-minor-mode c/code-elixir-mode
   "Mode for working with Elixir code."
@@ -19,6 +56,16 @@
     (prettify-symbols-mode -1)))
 
 (add-hook 'elixir-ts-mode-hook #'c/code-elixir-mode)
+
+(use-package inf-elixir
+  :defer t
+  :config
+  (add-hook 'elixir-mode-hook 'inf-elixir-minor-mode))
+
+(defun c/elixir-run-phx ()
+  (interactive)
+  (inf-elixir-project "iex -S mix phx.server"))
+
 
 ;;   (setq lsp-clients-elixir-server-executable '("~/dev/elixir-ls/release/language_server.sh"))
 ;;   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.elixir_ls\\'")
