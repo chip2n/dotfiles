cc4aeabd19b6a0cceafa835df5f9d9171d403272
chip2n <andreas@arvidsson.io>
2024-05-19 07:33:33 +0200

    Update guix config

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -4,7 +4,7 @@
   (name 'chip)
   (url "https://github.com/chip2n/guix-channel.git")
   (branch "main")
-  (commit "2650a7c0227ba71cecec254b0759cbc16b29c109"))
+  (commit "b155d6308322e7155509a0f50a2eb81b5af8637f"))
  ;; Guix channel
  (channel
   (name 'guix)
@@ -15,4 +15,14 @@
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
     (openpgp-fingerprint
-     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ ;; NonGuix channel
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  ;; Enable signature verification:
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
