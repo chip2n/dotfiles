52e87aeb28235a03eae682f5d2fc407d9afd10cf
chip2n <andreas@arvidsson.io>
2024-06-08 11:11:56 +0200

    Disable line numbers in emacs

--- .emacs.d/src/chip-theme-desktop.el
+++ .emacs.d/src/chip-theme-desktop.el
@@ -25,7 +25,7 @@
 (require 'chip-theme-base)
 
 ;; Show line numbers where appropriate
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+;; (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
 ;; Use minimum of 3 characters to avoid frequent resizing during window height change (e.g. during M-x)
 (setq-default display-line-numbers-width 3)
