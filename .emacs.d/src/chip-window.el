559f32e4d350d39ad29176f71fb4912762db680a
chip2n <andreas@arvidsson.io>
2024-06-20 06:52:42 +0200

    Tweak window splitting functionality

--- .emacs.d/src/chip-window.el
+++ .emacs.d/src/chip-window.el
@@ -292,6 +292,22 @@
 
 ;; (advice-add 'window-splittable-p :before-while #'do-not-split-more-than-two-windows)
 
+(setq split-height-threshold 120
+      split-width-threshold 160)
+
+(defun my-split-window-sensibly (&optional window)
+    "replacement `split-window-sensibly' function which prefers vertical splits"
+    (interactive)
+    (let ((window (or window (selected-window))))
+        (or (and (window-splittable-p window t)
+                 (with-selected-window window
+                     (split-window-right)))
+            (and (window-splittable-p window)
+                 (with-selected-window window
+                     (split-window-below))))))
+
+(setq split-window-preferred-function #'my-split-window-sensibly)
+
 (provide 'chip-window)
 
 ;;; chip-window.el ends here
