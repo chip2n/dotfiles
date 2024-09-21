8f17df89d6c3327702a7d67486b8058811760d2f
chip2n <andreas@arvidsson.io>
2024-09-21 18:32:52 +0200

    Better keybinding for embrace

--- .emacs.d/src/chip-meow.el
+++ .emacs.d/src/chip-meow.el
@@ -140,7 +140,7 @@
 
 (use-package embrace
   :straight (:type git :host github :repo "cute-jumper/embrace.el")
-  :bind (("C-M-s-#" . embrace-commander))
+  :bind (("C-c ," . embrace-commander))
   :config
   (add-hook 'org-mode-hook 'embrace-org-mode-hook)
   (defun embrace-markdown-mode-hook ()
