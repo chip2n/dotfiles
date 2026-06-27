88c0bbd31738620dc4800c6b98de7970150a413f
chip2n <andreas@arvidsson.io>
2026-06-27 12:01:13 +0200

    Update zig version in nix config

--- nixos/home/zig.nix
+++ nixos/home/zig.nix
@@ -2,7 +2,7 @@
 
 {
   home.packages = [
-    inputs.zig.packages.${pkgs.system}."master-2026-02-16"
+    inputs.zig.packages.${pkgs.system}."master-2026-04-01"
     inputs.zls.packages.${pkgs.system}.zls
   ];
 }
